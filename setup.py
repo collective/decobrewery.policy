d6f9b58d8e4c823f1919bd5fb805cf6e246fca5a
Rob Gietema <rob@fourdigits.nl>
2010-08-26 10:09:17 +0000

    Initial checkin of the policy.
    svn path=/decobrewery/decobrewery.policy/trunk/; revision=124200

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='decobrewery.policy',
+      version=version,
+      description="Deco Brewery Policy",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Rob Gietema',
+      author_email='rob@fourdigits.nl',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['decobrewery'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'decobrewery.xdvtheme',
+          'decobrewery.contenttypes',
+      ],
+      entry_points="""
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
